82a692265dd91f1311ffaba0c9c327484b9c4153
Glitch (topic-04-lab-ml-exercises) <none>
2024-02-08 14:04:55 +0000

    🌫️🧀 Updated with Glitch

---
+++ controllers/dashboard.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import logger from "../utils/logger.js";
+import playlistStore from "../models/playlist-store.js";
+import { v4 as uuidv4 } from 'uuid';
+
+const dashboard = {
+  createView(request, response) {
+    logger.info("Dashboard page loading!");
+    
+    const viewData = {
+      title: "Playlist App Dashboard",
+      playlists: playlistStore.getAllPlaylists()
+    };
+    
+    logger.debug(viewData.playlists);
+    
+    response.render('dashboard', viewData);
+  },
+  
+  addPlaylist(request, response) {
+    const timestamp = new Date();
+    
+    const newPlaylist = {
+      id: uuidv4(),
+      title: request.body.title,
+      songs: [],
+      date: timestamp
+    };
+    playlistStore.addPlaylist(newPlaylist);
+    response.redirect('/dashboard');
+  },
+};
+
+export default dashboard;
